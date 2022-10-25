dd84e151a0b349785e6c9192eaedd26953bf524c
hansa98 <hansa@vt.edu>
2022-10-25 13:04:32 -0400

    Lab1

---
+++ src/Lab1.java
@@ -0,0 +1,20 @@
+public class Lab1 {
+
+	public static void main(String[] args) {
+		boolean boo=true;
+		byte maxByte = 127;
+		short minShort = -32768;
+		int maxInt = 2147483647;
+		long javaFlex = 3000000000L;
+		float pie = 3.14159f;
+		double doubleYum = 3.14159 * 2;
+		System.out.println(boo);
+		System.out.println(maxByte);
+		System.out.println(minShort);
+		System.out.println(maxInt);
+		System.out.println(javaFlex);
+		System.out.println(pie);
+		System.out.println(doubleYum);
+	}
+
+}
